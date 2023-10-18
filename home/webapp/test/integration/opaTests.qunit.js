901f49db51e16daad034264631b133142c8f189c
rogerfafe <rogerfafe.tagss@gmail.com>
2023-10-18 14:11:16 -0300

    primeiro commit

---
+++ home/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["br/com/home/home/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ home/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"brcomhome/home/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
